4f371e81dfaaf6d7194f2f98956abc29d411409c
ranulagihara <ranulaw@opusxenta.com>
2024-03-17 06:48:37 +0530

    Taking turns will able to add O after a  X

--- react-game/src/App.js
+++ react-game/src/App.js
@@ -2,12 +2,15 @@ import { useState } from "react";
 import Square from "./components/Square";
 
 export default function Board() {
+  
+  const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
 
   function handleClick(i) {
     const nextSquares = squares.slice();
-    nextSquares[i] = "X";
+    xIsNext? nextSquares[i] = "X":nextSquares[i] = "O";
     setSquares(nextSquares);
+    setXIsNext(!xIsNext);
   }
 
   return (
